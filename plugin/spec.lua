43b27831eb1f87660d3eb5668281bb111b7c4249
Lucien Greathouse <me@lpghatguy.com>
2018-06-24 20:29:51 -0700

    Update Lemur and TestEZ

--- plugin/spec.lua
+++ plugin/spec.lua
@@ -14,34 +14,6 @@ package.path = package.path .. ";?/init.lua"
 -- If this fails, make sure you've run `lua bin/install-dependencies.lua` first!
 local lemur = require("modules.lemur")
 
---[[
-	Collapses ModuleScripts named 'init' into their parent folders.
-
-	This is the same result as the collapsing mechanism from Rojo.
-]]
-local function collapse(root)
-	local init = root:FindFirstChild("init")
-	if init then
-		init.Name = root.Name
-		init.Parent = root.Parent
-
-		for _, child in ipairs(root:GetChildren()) do
-			child.Parent = init
-		end
-
-		root:Destroy()
-		root = init
-	end
-
-	for _, child in ipairs(root:GetChildren()) do
-		if child:IsA("Folder") then
-			collapse(child)
-		end
-	end
-
-	return root
-end
-
 -- Create a virtual Roblox tree
 local habitat = lemur.Habitat.new()
 
@@ -51,12 +23,10 @@ Root.Name = "Root"
 
 -- Load all of the modules specified above
 for _, module in ipairs(LOAD_MODULES) do
-	local container = lemur.Instance.new("Folder", Root)
+	local container = habitat:loadFromFs(module[1])
 	container.Name = module[2]
-	habitat:loadFromFs(module[1], container)
+	container.Parent = Root
 end
-
-collapse(Root)
 
 -- Load TestEZ and run our tests
 local TestEZ = habitat:require(Root.TestEZ)
