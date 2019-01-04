02a3da111a37aa15bf9bad902bb8efa6c15dc79b
Lucien Greathouse <me@lpghatguy.com>
2019-01-04 14:55:53 -0800

    plugin: Fix test runner for new TestEZ

--- plugin/spec.lua
+++ plugin/spec.lua
@@ -9,7 +9,7 @@ local habitat, modules = loadEnvironment()
 -- Load TestEZ and run our tests
 local TestEZ = habitat:require(modules.TestEZ)
 
-local results = TestEZ.TestBootstrap:run(modules.Rojo, TestEZ.Reporters.TextReporter)
+local results = TestEZ.TestBootstrap:run({modules.Rojo}, TestEZ.Reporters.TextReporter)
 
 -- Did something go wrong?
 if results.failureCount > 0 then
