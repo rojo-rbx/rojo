83a0ae673c267ea5b36d306f9c126722dbfe5f7d
Lucien Greathouse <me@lpghatguy.com>
2019-04-04 21:20:00 -0700

    0.5.0-alpha.9

--- plugin/src/Config.lua
+++ plugin/src/Config.lua
@@ -1,6 +1,6 @@
 return {
 	codename = "Epiphany",
-	version = {0, 5, 0, "-alpha.8"},
+	version = {0, 5, 0, "-alpha.9"},
 	expectedServerVersionString = "0.5.0 or newer",
 	protocolVersion = 2,
 	defaultHost = "localhost",
