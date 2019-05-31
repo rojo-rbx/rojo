075b6cca30f12bc5d087308e15114b397d7e4ae5
Lucien Greathouse <me@lpghatguy.com>
2019-05-30 18:37:56 -0700

    Use new rbx_dom_lua API

--- plugin/src/setCanonicalProperty.lua
+++ plugin/src/setCanonicalProperty.lua
@@ -3,30 +3,32 @@ local RbxDom = require(script:FindFirstAncestor("Rojo").RbxDom)
 --[[
 	Attempts to set a property on the given instance.
 ]]
-local function setCanonicalProperty(instance, key, value)
-	-- If we don't have permissions to access this value at all, we can skip it.
-	local readSuccess, existingValue = RbxDom.readProperty(instance, key)
+local function setCanonicalProperty(instance, propertyName, value)
+	local descriptor = RbxDom.findCanonicalPropertyDescriptor(instance.ClassName, propertyName)
 
-	if not readSuccess then
-		if existingValue.kind == RbxDom.Error.Kind.UnknownProperty
-			or existingValue.kind == RbxDom.Error.Kind.PropertyNotReadable then
-			-- this is fine
-			return false
-		end
-
-		-- If we don't have permission to write a property, we just silently
-		-- ignore it.
-		if existingValue.kind == RbxDom.Error.Kind.Roblox and existingValue.extra:find("lacking permission") then
-			return false
-		end
-
-		error(("Invalid property %s on class %s: %s"):format(tostring(key), instance.ClassName, tostring(existingValue)), 2)
+	-- We can skip unknown properties; they're not likely reflected to Lua.
+	--
+	-- A good example of a property like this is `Model.ModelInPrimary`, which
+	-- is serialized but not reflected to Lua.
+	if descriptor == nil then
+		return false, "unknown property"
 	end
 
-	local writeSuccess, err = RbxDom.writeProperty(instance, key, value)
+	if descriptor.scriptability == "None" or descriptor.scriptability == "Read" then
+		return false, "unwritable property"
+	end
 
-	if not writeSuccess then
-		error(("Cannot set property %s on class %s: %s"):format(tostring(key), instance.ClassName, tostring(err)), 2)
+	local success, err = descriptor:write(instance, value)
+
+	if not success then
+		-- If we don't have permission to write a property, we just silently
+		-- ignore it.
+		if err.kind == RbxDom.Error.Kind.Roblox and err.extra:find("lacking permission") then
+			return false, "permission error"
+		end
+
+		local message = ("Invalid property %s.%s: %s"):format(descriptor.className, descriptor.name, tostring(err))
+		error(message, 2)
 	end
 
 	return true
