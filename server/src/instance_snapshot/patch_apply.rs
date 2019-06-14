272b7bfdf86fe3f7f44e6fcc22d7039101e60ce8
Lucien Greathouse <me@lpghatguy.com>
2019-06-14 11:44:02 -0700

    Rough apply_patch function

--- server/src/instance_snapshot/patch_apply.rs
+++ server/src/instance_snapshot/patch_apply.rs
@@ -1,16 +1,32 @@
-use std::collections::{HashMap, HashSet};
+use rbx_dom_weak::RbxTree;
 
-use rbx_dom_weak::{RbxTree, RbxId, RbxInstance};
-
-use super::{
-    snapshot::InstanceSnapshot,
-    patch::{PatchSet, PatchChildren, PatchChildrenEntry, PatchProperties},
-};
+use super::patch::PatchSet;
 
 pub fn apply_patch(
-    tree: &RbxTree,
-    id: RbxId,
+    tree: &mut RbxTree,
     patch_set: &PatchSet,
 ) {
+    for child_patch in &patch_set.children {
+        // TODO
+    }
 
+    for prop_patch in &patch_set.properties {
+        let instance = tree.get_instance_mut(prop_patch.id)
+            .expect("Instance referred to by patch does not exist.");
+
+        if let Some(name) = &prop_patch.changed_name {
+            instance.name = name.clone();
+        }
+
+        for (key, property_entry) in &prop_patch.changed_properties {
+            match property_entry {
+                Some(value) => {
+                    instance.properties.insert(key.clone(), value.clone());
+                }
+                None => {
+                    instance.properties.remove(key);
+                }
+            }
+        }
+    }
 }
