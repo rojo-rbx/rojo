e04b37b4116f5472c1db33b97774854560cc763b
Lucien Greathouse <me@lpghatguy.com>
2019-07-25 16:59:03 -0700

    Propose new error type

--- server/src/imfs/error.rs
+++ server/src/imfs/error.rs
@@ -9,6 +9,19 @@ use failure::Fail;
 pub type FsResult<T> = Result<T, FsError>;
 pub use io::ErrorKind as FsErrorKind;
 
+// TODO: New error type that contains errors specific to our application,
+// wrapping io::Error either directly or through another error type that has
+// path information.
+//
+// It's possible that we should hoist up the path information one more level, or
+// destructure/restructure information to hoist the path out of FsError and just
+// embed io::Error?
+pub enum ImfsError {
+    NotFound,
+    WrongKind,
+    Fs(FsError),
+}
+
 /// A wrapper around io::Error that also attaches the path associated with the
 /// error.
 #[derive(Debug, Fail)]
