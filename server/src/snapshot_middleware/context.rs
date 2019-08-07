81df05aae6773e0f99410b8511318ce60af6b673
Lucien Greathouse <me@lpghatguy.com>
2019-08-07 14:11:55 -0700

    Add snapshot context object for Imfs in the future

--- server/src/snapshot_middleware/context.rs
+++ server/src/snapshot_middleware/context.rs
@@ -1,5 +1,7 @@
-pub struct SnapshotContext {
+pub struct InstanceSnapshotContext {
     /// Empty struct that will be used later to fill out required Lua state for
     /// user plugins.
     pub plugin_context: Option<()>,
 }
+
+pub struct ImfsSnapshotContext;
