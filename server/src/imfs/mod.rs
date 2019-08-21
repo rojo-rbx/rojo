8f54591b6ea5ddf3deeb61f160ee255ab14c6195
Lucien Greathouse <me@lpghatguy.com>
2019-08-20 18:04:20 -0700

    Add ImfsWatcher trait, I think this will be merged with ImfsFetcher

--- server/src/imfs/mod.rs
+++ server/src/imfs/mod.rs
@@ -5,6 +5,7 @@ mod legacy;
 mod noop_fetcher;
 mod real_fetcher;
 mod snapshot;
+mod watcher;
 
 pub use legacy::*;
 pub use error::*;
@@ -16,4 +17,5 @@ pub mod new {
     pub use super::real_fetcher::*;
     pub use super::noop_fetcher::*;
     pub use super::snapshot::*;
+    pub use super::watcher::*;
 }
