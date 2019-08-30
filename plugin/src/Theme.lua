17e3eb3a8cbf41dcbdd5d2aa3e2dcc4e721fc676
Jaguar <48777426+Jaguar-515@users.noreply.github.com>
2019-08-30 16:10:59 -0700

    Update Theme.lua
    Added if/else statement to check user is on dark mode or not

--- plugin/src/Theme.lua
+++ plugin/src/Theme.lua
@@ -11,10 +11,35 @@ local Theme = {
 	LightTextColor = Color3.fromRGB(160, 160, 160),
 }
 
-setmetatable(Theme, {
+--[[ local DarkTheme = {
+
+	Little unsure on how to change the color of the background on the plugin for dark mode...
+
+	ButtonFont = Enum.Font.GothamSemibold,
+	InputFont = Enum.Font.Code,
+	TitleFont = Enum.Font.GothamBold,
+	MainFont = Enum.Font.Gotham,
+	
+	AccentColor = Color3.fromRGB(89, 88, 88),
+	AccentLightColor = Color3.fromRGB(255, 146, 145),
+	PrimaryColor = Color3.fromRGB(53, 53, 53),
+	SecondaryColor = Color3.fromRGB(103, 103, 103),
+	LightTextColor = Color3.fromRGB(103, 103, 103)
+} ]]--
+
+if Enum.UITheme == Enum.UITheme.Light then
+	setmetatable(Theme, {
 	__index = function(_, key)
 		error(("%s is not a valid member of Theme"):format(key), 2)
 	end
-})
-
-return Theme
+	})
+	return Theme
+--[[ else
+	setmetatable(DarkTheme, {
+		__index = function(_, key)
+			error(("%s is not a valid member of Theme"):format(key), 2)
+		end
+	})
+	return DarkTheme
+]]--
+end 
