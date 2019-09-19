b31ad4b1f835b0c59b2f3a1e285c840d753b16ce
Lucien Greathouse <me@lpghatguy.com>
2019-09-19 13:58:53 -0700

    Update version to 0.6.0-dev

--- plugin/src/Config.lua
+++ plugin/src/Config.lua
@@ -1,7 +1,7 @@
 return {
 	codename = "Epiphany",
-	version = {0, 5, 0},
-	expectedServerVersionString = "0.5.0 or newer",
+	version = {0, 6, 0, "-dev"},
+	expectedServerVersionString = "0.6.0 or newer",
 	protocolVersion = 3,
 	defaultHost = "localhost",
 	defaultPort = 34872,
