b2fc6c165b0814da77fe6c54a49246920e704490
Lucien Greathouse <me@lpghatguy.com>
2019-09-30 17:22:54 -0700

    Improve patch application test

--- src/snapshot/tests/apply.rs
+++ src/snapshot/tests/apply.rs
@@ -1,10 +1,13 @@
+use std::collections::HashMap;
+
 use insta::assert_yaml_snapshot;
-use rbx_dom_weak::RbxInstanceProperties;
+use rbx_dom_weak::{RbxId, RbxInstanceProperties, RbxValue};
+use serde::Serialize;
 
 use rojo_insta_ext::RedactionMap;
 
 use crate::snapshot::{
-    apply_patch_set, InstancePropertiesWithMeta, PatchSet, PatchUpdate, RojoTree,
+    apply_patch_set, InstanceMetadata, InstancePropertiesWithMeta, PatchSet, PatchUpdate, RojoTree,
 };
 
 #[test]
@@ -27,7 +30,9 @@ fn set_name_and_class_name() {
 
     let applied_patch_set = apply_patch_set(&mut tree, patch_set);
 
-    // TODO: Snapshot tree, requires RojoTree: Serialize (but not Deserialize!)
+    let tree_view = view_tree(&tree);
+    let tree_value = redactions.redacted_yaml(tree_view);
+    assert_yaml_snapshot!(tree_value);
 
     let applied_patch_value = redactions.redacted_yaml(applied_patch_set);
     assert_yaml_snapshot!(applied_patch_value);
@@ -42,4 +47,36 @@ fn empty_tree() -> RojoTree {
         },
         metadata: Default::default(),
     })
+}
+
+#[derive(Debug, Serialize)]
+struct InstanceView {
+    id: RbxId,
+    name: String,
+    class_name: String,
+    properties: HashMap<String, RbxValue>,
+    metadata: InstanceMetadata,
+    children: Vec<InstanceView>,
+}
+
+fn view_tree(tree: &RojoTree) -> InstanceView {
+    view_instance(tree, tree.get_root_id())
+}
+
+fn view_instance(tree: &RojoTree, id: RbxId) -> InstanceView {
+    let instance = tree.get_instance(id).unwrap();
+
+    InstanceView {
+        id: instance.id(),
+        name: instance.name().to_owned(),
+        class_name: instance.class_name().to_owned(),
+        properties: instance.properties().clone(),
+        metadata: instance.metadata().clone(),
+        children: instance
+            .children()
+            .iter()
+            .copied()
+            .map(|id| view_instance(tree, id))
+            .collect(),
+    }
 }
