cf62ee5ccbeda547ff1faea2b685e9fbe5b7a1fe
Lucien Greathouse <me@lpghatguy.com>
2019-10-03 15:54:49 -0700

    plugin: Add Id and ParentId to ApiInstance type definitions

--- plugin/src/Types.lua
+++ plugin/src/Types.lua
@@ -15,6 +15,8 @@ local ApiInstanceMetadata = t.interface({
 })
 
 local ApiInstance = t.interface({
+	Id = RbxId,
+	ParentId = t.optional(RbxId),
 	Name = t.string,
 	ClassName = t.string,
 	Properties = t.map(t.string, ApiValue),
