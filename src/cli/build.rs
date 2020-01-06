5d1709ecf2c2eb0cb1d3e85da320b6c9b069c18b
Lucien Greathouse <me@lpghatguy.com>
2020-01-05 23:21:25 -0800

    Refactor build command to reproduce model more easily

--- src/cli/build.rs
+++ src/cli/build.rs
@@ -9,6 +9,7 @@ use crate::{
     cli::BuildCommand,
     common_setup,
     project::ProjectError,
+    snapshot::RojoTree,
     vfs::{RealFetcher, Vfs, WatchMode},
 };
 
@@ -71,18 +72,25 @@ pub fn build(options: BuildCommand) -> Result<(), BuildError> {
 }
 
 fn build_inner(options: BuildCommand) -> Result<(), Error> {
-    let output_kind = detect_output_kind(&options).ok_or(Error::UnknownOutputKind)?;
-
-    log::debug!("Hoping to generate file of type {:?}", output_kind);
-
     log::trace!("Constructing in-memory filesystem");
     let vfs = Vfs::new(RealFetcher::new(WatchMode::Disabled));
 
     let (_maybe_project, tree) = common_setup::start(&options.absolute_project(), &vfs);
+
+    write_model(&tree, &options)?;
+
+    log::trace!("Done!");
+
+    Ok(())
+}
+
+fn write_model(tree: &RojoTree, options: &BuildCommand) -> Result<(), Error> {
+    let output_kind = detect_output_kind(&options).ok_or(Error::UnknownOutputKind)?;
+    log::debug!("Hoping to generate file of type {:?}", output_kind);
+
     let root_id = tree.get_root_id();
 
     log::trace!("Opening output file for write");
-
     let file = File::create(&options.output).context(Io)?;
     let mut file = BufWriter::new(file);
 
@@ -120,8 +128,6 @@ fn build_inner(options: BuildCommand) -> Result<(), Error> {
     }
 
     file.flush().context(Io)?;
-
-    log::trace!("Done!");
 
     Ok(())
 }
