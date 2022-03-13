f8e8fbc255052f0ef7f20196366523f07d884b92
Alex Stone <stone@filamentgames.com>
2022-03-12 18:52:49 -0600

    Properly handle IEEE 754 tokens in arrays

--- plugin/http/Encode.lua
+++ plugin/http/Encode.lua
@@ -6,13 +6,14 @@ local Encode = {}
 function Encode.jsonEncode(object)
 	-- Encode json 5 IEEE 754 tokens
 	local body = HttpService:JSONEncode(object)
-	body = body:gsub(":[%c%s]*\"([\-NI]%a+)\"", ":%1")
+	body = body:gsub("([:\[\,][%c%s]*)\"([\[\-NI]%a+)\"", "%1%2")
 	return body
 end
 
 function Encode.jsonDecode(source)
 	-- Decode json 5 IEEE 754 tokens
-	local body = source:gsub(":[%c%s]*([\-NI]%a+)", ":\"%1\"")
+	local body = source:gsub("([:\[\,][%c%s]*)([\-NI]%a+)", "%1\"%2\"")
+	print(body)
 	return HttpService:JSONDecode(body)
 end
 
