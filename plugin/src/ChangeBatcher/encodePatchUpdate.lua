4e19227390a4836359dcfc58d05bf21a57a26f89
unix_system <4990534+unix-system@users.noreply.github.com>
2022-04-18 23:27:58 +0100

    Change logic to recreate when MeshId is changed

--- plugin/src/ChangeBatcher/encodePatchUpdate.lua
+++ plugin/src/ChangeBatcher/encodePatchUpdate.lua
@@ -8,28 +8,33 @@ return function(instance, instanceId, properties)
 	local update = {
 		id = instanceId,
 		changedProperties = {},
+		requiresRecreate = false
 	}
 
 	for propertyName in pairs(properties) do
 		if propertyName == "Name" then
 			update.changedName = instance.Name
-		else
-			local descriptor = RbxDom.findCanonicalPropertyDescriptor(instance.ClassName, propertyName)
-
-			if not descriptor then
-				Log.debug("Could not sync back property {:?}.{}", instance, propertyName)
-				continue
-			end
-
-			local encodeSuccess, encodeResult = encodeProperty(instance, propertyName, descriptor)
-
-			if not encodeSuccess then
-				Log.debug("Could not sync back property {:?}.{}: {}", instance, propertyName, encodeResult)
-				continue
-			end
-
-			update.changedProperties[propertyName] = encodeResult
+		elseif propertyName == "MeshId" then
+			update.requiresRecreate = true
+		elseif propertyName == "ClassName" then
+			update.requiresRecreate, update.changedClassName = true, instance.ClassName
 		end
+		
+		local descriptor = RbxDom.findCanonicalPropertyDescriptor(instance.ClassName, propertyName)
+
+		if not descriptor then
+			Log.debug("Could not sync back property {:?}.{}", instance, propertyName)
+			continue
+		end
+
+		local encodeSuccess, encodeResult = encodeProperty(instance, propertyName, descriptor)
+
+		if not encodeSuccess then
+			Log.debug("Could not sync back property {:?}.{}: {}", instance, propertyName, encodeResult)
+			continue
+		end
+
+		update.changedProperties[propertyName] = encodeResult
 	end
 
 	if next(update.changedProperties) == nil and update.changedName == nil then
