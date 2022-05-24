58d30859615cab965206ae955dbc76dc2c8c9104
boatbomber <arbitrarysoandso@gmail.com>
2022-05-24 16:18:06 -0400

    Fix broken audio, thanks Roblox

--- plugin/src/playSound.lua
+++ plugin/src/playSound.lua
@@ -1,17 +1,22 @@
---[[
-	Roblox plugins have painfully limited audio capabilities.
-	https://devforum.roblox.com/t/allow-sounds-to-be-played-in-studio/403644
---]]
-
-local SoundService = game:GetService("SoundService")
+-- Roblox decided that sounds only play in Edit mode when parented to a plugin widget, for some reason
+local plugin = plugin or script:FindFirstAncestorWhichIsA("Plugin")
+local widget = plugin:CreateDockWidgetPluginGui("Rojo_soundPlayer", DockWidgetPluginGuiInfo.new(
+	Enum.InitialDockState.Float,
+	false, true,
+	10, 10,
+	10, 10
+))
+widget.Name = "Rojo_soundPlayer"
+widget.Title = "Rojo Sound Player"
 
 return function(soundId)
 	local sound = Instance.new("Sound")
 	sound.SoundId = soundId
+	sound.Parent = widget
 
 	sound.Ended:Connect(function()
 		sound:Destroy()
 	end)
 
-	SoundService:PlayLocalSound(sound)
+	sound:Play()
 end
