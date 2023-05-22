ca558a64693fb2727aa0c9c372f2e68c6f249cee
Kenneth Loeffler <kenloef@gmail.com>
2023-05-22 04:32:44 -0700

    Don't borrow start_path in ServeSession::new
    fix clippy::needless_borrow

--- src/serve_session.rs
+++ src/serve_session.rs
@@ -126,7 +126,7 @@ impl ServeSession {
         let instance_context = InstanceContext::default();
 
         log::trace!("Generating snapshot of instances from VFS");
-        let snapshot = snapshot_from_vfs(&instance_context, &vfs, &start_path)?;
+        let snapshot = snapshot_from_vfs(&instance_context, &vfs, start_path)?;
 
         log::trace!("Computing initial patch set");
         let patch_set = compute_patch_set(snapshot, &tree, root_id);
