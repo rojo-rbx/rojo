7322c7e08bbceb2e732450f89f0099e54da28dee
boatbomber <zack@boatbomber.com>
2023-07-09 22:25:10 -0700

    Add postcommit hooks too

--- plugin/src/Reconciler/init.lua
+++ plugin/src/Reconciler/init.lua
@@ -18,6 +18,7 @@ function Reconciler.new(instanceMap)
 		-- Tracks all of the instances known by the reconciler by ID.
 		__instanceMap = instanceMap,
 		__precommitCallbacks = {},
+		__postcommitCallbacks = {},
 	}
 
 	return setmetatable(self, Reconciler)
@@ -37,6 +38,20 @@ function Reconciler:hookPrecommit(callback: (patch: any, instanceMap: any) -> ()
 	end
 end
 
+function Reconciler:hookPostcommit(callback: (patch: any, instanceMap: any, unappliedPatch: any) -> ()): () -> ()
+	table.insert(self.__postcommitCallbacks, callback)
+
+	return function()
+		-- Remove the callback from the list
+		for i, cb in self.__postcommitCallbacks do
+			if cb == callback then
+				table.remove(self.__postcommitCallbacks, i)
+				break
+			end
+		end
+	end
+end
+
 function Reconciler:applyPatch(patch)
 	for _, callback in self.__precommitCallbacks do
 		local success, err = pcall(callback, patch, self.__instanceMap)
@@ -45,7 +60,16 @@ function Reconciler:applyPatch(patch)
 		end
 	end
 
-	return applyPatch(self.__instanceMap, patch)
+	local unappliedPatch = applyPatch(self.__instanceMap, patch)
+
+	for _, callback in self.__postcommitCallbacks do
+		local success, err = pcall(callback, patch, self.__instanceMap, unappliedPatch)
+		if not success then
+			Log.warn("Postcommit hook errored: {}", err)
+		end
+	end
+
+	return unappliedPatch
 end
 
 function Reconciler:hydrate(virtualInstances, rootId, rootInstance)
