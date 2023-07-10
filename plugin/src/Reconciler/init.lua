2da7bf3b0be53c9b01e9dc778c9b6f648b2c5d23
boatbomber <zack@boatbomber.com>
2023-07-09 18:31:29 -0700

    Add precommit hooks to reconciler

--- plugin/src/Reconciler/init.lua
+++ plugin/src/Reconciler/init.lua
@@ -3,6 +3,9 @@
 	and mutating the Roblox DOM.
 ]]
 
+local Packages = script.Parent.Parent.Packages
+local Log = require(Packages.Log)
+
 local applyPatch = require(script.applyPatch)
 local hydrate = require(script.hydrate)
 local diff = require(script.diff)
@@ -14,12 +17,34 @@ function Reconciler.new(instanceMap)
 	local self = {
 		-- Tracks all of the instances known by the reconciler by ID.
 		__instanceMap = instanceMap,
+		__precommitCallbacks = {},
 	}
 
 	return setmetatable(self, Reconciler)
 end
 
+function Reconciler:hookPrecommit(callback: (patch: any, instanceMap: any) -> ()): () -> ()
+	table.insert(self.__precommitCallbacks, callback)
+
+	return function()
+		-- Remove the callback from the list
+		for i, cb in self.__precommitCallbacks do
+			if cb == callback then
+				table.remove(self.__precommitCallbacks, i)
+				break
+			end
+		end
+	end
+end
+
 function Reconciler:applyPatch(patch)
+	for _, callback in self.__precommitCallbacks do
+		local success, err = pcall(callback, patch, self.__instanceMap)
+		if not success then
+			Log.warn("Precommit hook errored: {}", err)
+		end
+	end
+
 	return applyPatch(self.__instanceMap, patch)
 end
 
