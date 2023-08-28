a5af0e89162c2f89d73183e232098584a99d6ef3
Dekkonot <git@dekkonot.com>
2023-08-27 21:05:01 -0700

    Add initial implementation of fetch api for client

--- plugin/src/ApiContext.lua
+++ plugin/src/ApiContext.lua
@@ -253,4 +253,18 @@ function ApiContext:open(id)
 		end)
 end
 
+function ApiContext:fetch(ids: {string})
+	local url = ("%s/api/fetch/%s"):format(self.__baseUrl, table.concat(ids, ","))
+	return Http.get(url)
+		:andThen(rejectFailedRequests)
+		:andThen(Http.Response.json)
+		:andThen(function(body)
+			if body.sessionId ~= self.__sessionId then
+				return Promise.reject("Server changed ID")
+			end
+
+			return body
+		end)
+end
+
 return ApiContext
--- plugin/src/Reconciler/reify.lua
+++ plugin/src/Reconciler/reify.lua
@@ -2,12 +2,16 @@
 	"Reifies" a virtual DOM, constructing a real DOM with the same shape.
 ]]
 
+local Rojo = script:FindFirstAncestor("Rojo")
+
 local invariant = require(script.Parent.Parent.invariant)
 local PatchSet = require(script.Parent.Parent.PatchSet)
 local setProperty = require(script.Parent.setProperty)
 local decodeValue = require(script.Parent.decodeValue)
 
-local reifyInner, applyDeferredRefs
+local Log = require(Rojo.Packages.Log)
+
+local reifyInner, fetchUnapplied, applyDeferredRefs
 
 local function reify(instanceMap, virtualInstances, rootId, parentInstance, serveSession)
 	-- Create an empty patch that will be populated with any parts of this reify
@@ -153,6 +157,58 @@ function applyDeferredRefs(instanceMap, deferredRefs, unappliedPatch)
 			markFailed(entry.id, entry.propertyName, entry.virtualValue)
 		end
 	end
+end
+
+function fetchInstances(idList, instanceMap, serveSession)
+	return serveSession.__apiContext:fetch(idList)
+		:andThen(function(body: {sessionId: string, path: string})
+			-- The endpoint `api/fetech/idlist` returns a table that contains
+			-- `sessionId` and `path`. The value of `path` is the name of a
+			-- file in the Content folder that may be loaded via `GetObjects`.
+			Log.debug("Loading assets for {}", idList)
+			local objects = game:GetObjects("rbxasset://" .. body.path)
+			-- `ReferentMap` is a folder that contains nothing but
+			-- ObjectValues which will be named after entries in `instanceMap`
+			-- and have their `Value` property point towards a new Instance
+			-- that it can be swapped out with. In turn, `reified` is a
+			-- container for all of the objects pointed to by those instances.
+			local map = objects[1]:FindFirstChild("ReferentMap")
+			local reified = objects[1]:FindFirstChild("Reified")
+			if map == nil then
+				invariant("The fetch endpoint returned a malformed folder: missing ReferentMap")
+			end
+			if reified == nil then
+				invariant("The fetch endpoint returned a malformed folder: missing Reified")
+			end
+			for _, entry in map:GetChildren() do
+				if entry:IsA("ObjectValue") then
+					local key, value = entry.Name, entry.Value
+					if value == nil or value.Parent ~= reified then
+						invariant("ReferentMap contained entry {} that was parented to an outside source", key)
+					else
+						-- This could be a problem if Roblox ever supports
+						-- parallel access to the DataModel but right now,
+						-- there's no way this results in a data race.
+						local oldInstance: Instance = instanceMap.fromIds[key]
+						instanceMap:insert(key, value)
+						Log.debug("Swapping Instance {} out", oldInstance:GetFullName())
+
+						local oldParent = oldInstance.Parent
+						local children = oldInstance:GetChildren()
+						for _, child in children do
+							child.Parent = value
+						end
+						value.Parent = oldParent
+
+						-- So long and thanks for all the fish :-)
+						oldInstance:Destroy()
+					end
+				else
+					invariant("ReferentMap entry `{}` was a `{}` and not an ObjectValue", entry.Name, entry.ClassName)
+				end
+			end
+
+		end)
 end
 
 return reify
