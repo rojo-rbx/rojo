e0c43801048ae7a41f15938dd43d4215afae3d41
Dekkonot <git@dekkonot.com>
2023-09-18 16:46:56 -0700

    Reference instances by key instead of name

--- plugin/src/Reconciler/fetchInstances.lua
+++ plugin/src/Reconciler/fetchInstances.lua
@@ -34,7 +34,7 @@ local function fetchInstances(idList, instanceMap, apiContext)
 						-- there's no way this results in a data race.
 						local oldInstance: Instance = instanceMap.fromIds[key]
 						instanceMap:insert(key, value)
-						Log.trace("Swapping Instance {} out", oldInstance:GetFullName())
+						Log.trace("Swapping Instance {} out", key)
 
 						local oldParent = oldInstance.Parent
 						local children = oldInstance:GetChildren()
