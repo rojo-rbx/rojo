5a0c24d2d1384c5c229dfaff34bf1a85bbfafd51
Micah <micah@uplift.games>
2023-10-27 13:48:00 -0700

    Trim extensions instead of just one

--- src/snapshot_middleware/util.rs
+++ src/snapshot_middleware/util.rs
@@ -16,7 +16,7 @@ pub fn match_trailing<'a>(input: &'a str, suffix: &str) -> Option<&'a str> {
 pub trait PathExt {
     fn file_name_ends_with(&self, suffix: &str) -> bool;
     fn file_name_trim_end<'a>(&'a self, suffix: &str) -> anyhow::Result<&'a str>;
-    fn file_name_trim_extension(&self) -> anyhow::Result<&str>;
+    fn file_name_trim_extensions(&self) -> anyhow::Result<&str>;
 }
 
 impl<P> PathExt for P
@@ -42,15 +42,46 @@ where
             .with_context(|| format!("Path did not end in {}: {}", suffix, path.display()))
     }
 
-    fn file_name_trim_extension(&self) -> anyhow::Result<&str> {
-        self.as_ref()
+    /// Returns the name of a file after all extensions have been removed.
+    fn file_name_trim_extensions(&self) -> anyhow::Result<&str> {
+        // I would love for this to be less verbose, but I'm not sure it's
+        // really possible this. It doesn't allocate, so it's no huge loss
+        // either way though.
+        let mut file_name = self
+            .as_ref()
             .file_stem()
             .and_then(|n| n.to_str())
-            .with_context(|| format!("file name of {} is invalid", self.as_ref().display()))
+            .with_context(|| format!("file name of {} is invalid", self.as_ref().display()))?;
+        while Path::new(file_name).extension().is_some() {
+            file_name = Path::new(file_name)
+                .file_stem()
+                .and_then(|n| n.to_str())
+                .with_context(|| format!("file name of {} is invalid", self.as_ref().display()))?;
+        }
+        Ok(file_name)
     }
 }
 
 // TEMP function until rojo 8.0, when it can be replaced with bool::default (aka false)
 pub fn emit_legacy_scripts_default() -> Option<bool> {
     Some(true)
+}
+
+#[test]
+fn file_name_trim_extensions_invalid() {
+    // Basic test to make sure that the loop for this function
+    // isn't infinite and that it works right
+    assert!(Path::new("").file_name_trim_extensions().is_err());
+
+    assert_eq!(Path::new("foo").file_name_trim_extensions().unwrap(), "foo");
+    assert_eq!(
+        Path::new("foo.bar").file_name_trim_extensions().unwrap(),
+        "foo"
+    );
+    assert_eq!(
+        Path::new("foo.bar.baz")
+            .file_name_trim_extensions()
+            .unwrap(),
+        "foo"
+    );
 }
