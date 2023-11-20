986e327c71278b3b912b738526b8f47daa1402a3
Micah <micah@uplift.games>
2023-11-20 10:22:00 -0800

    Updating hasher implementation

--- src/snapshot/diff/hash.rs
+++ src/snapshot/diff/hash.rs
@@ -2,16 +2,13 @@
 
 use blake3::{Hash, Hasher};
 use rbx_dom_weak::{
-    types::{Ref, Variant},
+    types::{Ref, Variant, Vector3},
     Instance, WeakDom,
 };
 
-use crate::snapshot::RojoTree;
-
 use std::collections::{HashMap, VecDeque};
 
-pub fn hash_tree(tree: &RojoTree) -> HashMap<Ref, Hash> {
-    let dom = tree.inner();
+pub fn hash_tree(dom: &WeakDom) -> HashMap<Ref, Hash> {
     let mut map: HashMap<Ref, Hash> = HashMap::new();
     let mut order = descendants(dom);
 
@@ -21,7 +18,6 @@ pub fn hash_tree(tree: &RojoTree) -> HashMap<Ref, Hash> {
     // return hash({ sort(foreach(inst.properties, hash)), sort(foreach(inst.children, get_hash_id)) })
     // end
     while let Some(referent) = order.pop() {
-        log::info!("processing {referent}");
         let inst = dom.get_by_ref(referent).unwrap();
         let hash = hash_inst(&mut prop_list, &map, inst);
 
@@ -88,7 +84,7 @@ fn descendants(dom: &WeakDom) -> Vec<Ref> {
 macro_rules! n_hash {
     ($hash:ident, $($num:expr),*) => {
         {$(
-            $hash.update(&$num.to_le_bytes());
+            $hash.update(&($num).to_le_bytes());
         )*}
     };
 }
@@ -106,15 +102,57 @@ fn hash_variant(hasher: &mut Hasher, value: &Variant) {
     match value {
         Variant::String(str) => hash!(hasher, str.as_bytes()),
         Variant::Bool(bool) => hash!(hasher, &[*bool as u8]),
-        Variant::Float32(n) => n_hash!(hasher, n),
+        Variant::Float32(n) => n_hash!(hasher, round(*n)),
         Variant::Float64(n) => n_hash!(hasher, n),
         Variant::Int32(n) => n_hash!(hasher, n),
         Variant::Int64(n) => n_hash!(hasher, n),
         Variant::BinaryString(bytes) => hash!(hasher, bytes.as_ref()),
-        Variant::Vector3(v3) => n_hash!(hasher, v3.x, v3.y, v3.z),
-        Variant::Vector2(v2) => n_hash!(hasher, v2.x, v2.y),
+        Variant::Vector3(v3) => vector_hash(hasher, *v3),
+        Variant::Vector2(v2) => n_hash!(hasher, round(v2.x), round(v2.y)),
+        Variant::Axes(a) => hash!(hasher, &[a.bits()]),
+        Variant::BrickColor(color) => n_hash!(hasher, *color as u16),
+        Variant::CFrame(cf) => {
+            vector_hash(hasher, cf.position);
+            vector_hash(hasher, cf.orientation.x);
+            vector_hash(hasher, cf.orientation.y);
+            vector_hash(hasher, cf.orientation.z);
+        }
+        Variant::Color3(color) => n_hash!(hasher, round(color.r), round(color.g), round(color.b)),
+        Variant::Color3uint8(color) => hash!(hasher, &[color.r, color.b, color.g]),
+        Variant::ColorSequence(seq) => {
+            let mut new = Vec::with_capacity(seq.keypoints.len());
+            for keypoint in &seq.keypoints {
+                new.push(keypoint);
+            }
+            new.sort_unstable_by(|a, b| round(a.time).partial_cmp(&round(b.time)).unwrap());
+            for keypoint in new {
+                n_hash!(
+                    hasher,
+                    round(keypoint.time),
+                    round(keypoint.color.r),
+                    round(keypoint.color.g),
+                    round(keypoint.color.b)
+                )
+            }
+        }
+        // TODO: Make this more ergonomic
+        Variant::Content(content) => {
+            let s: &str = content.as_ref();
+            hash!(hasher, s.as_bytes())
+        }
+        Variant::Enum(e) => n_hash!(hasher, e.to_u32()),
+        Variant::Faces(f) => hash!(hasher, &[f.bits()]),
 
         // TODO: Add the rest
-        _ => (),
+        // Hashing UniqueId properties doesn't make sense
+        Variant::UniqueId(_) | _ => (),
     }
+}
+
+fn vector_hash(hasher: &mut Hasher, vector: Vector3) {
+    n_hash!(hasher, round(vector.x), round(vector.y), round(vector.z))
+}
+
+fn round(float: f32) -> f32 {
+    (float * 10.0).round() / 10.0
 }
