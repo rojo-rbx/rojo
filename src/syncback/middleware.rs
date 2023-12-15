981829151bfd6a849fbab1a0fc718cb247d61711
Micah <micah@uplift.games>
2023-12-15 12:47:37 -0800

    Make a myriad of changes to syncback Implement project middleware

--- src/syncback/middleware.rs
+++ src/syncback/middleware.rs
@@ -7,13 +7,13 @@ use rbx_dom_weak::{
 
 use crate::{
     resolution::UnresolvedValue,
-    snapshot::{InstanceMetadata, InstanceSnapshot, InstanceWithMeta},
+    snapshot::{InstanceMetadata, InstanceSnapshot, InstanceWithMeta, InstigatingSource},
     snapshot_middleware::{DirectoryMetadata, Middleware, ScriptType},
+    Project, ProjectNode,
 };
 
 use super::{FsSnapshot, SyncbackSnapshot};
 
-#[derive(Debug)]
 pub struct SyncbackReturn<'new, 'old> {
     pub inst_snapshot: InstanceSnapshot,
     pub fs_snapshot: FsSnapshot,
@@ -26,11 +26,42 @@ pub fn syncback_middleware<'new, 'old>(
     middleware: Middleware,
 ) -> SyncbackReturn<'new, 'old> {
     match middleware {
+        Middleware::Project => syncback_project(snapshot),
         Middleware::ModuleScript => syncback_script(ScriptType::Module, snapshot),
         Middleware::ClientScript => syncback_script(ScriptType::Client, snapshot),
         Middleware::ServerScript => syncback_script(ScriptType::Server, snapshot),
         Middleware::Dir => syncback_dir(snapshot),
         _ => panic!("unsupported instance middleware {:?}", middleware),
+    }
+}
+
+pub fn get_best_middleware(inst: &Instance) -> Middleware {
+    match inst.class.as_str() {
+        "Folder" => Middleware::Dir,
+        // TODO this should probably just be rbxm
+        "Model" => Middleware::Rbxmx,
+        "Script" => {
+            if inst.children().len() == 0 {
+                Middleware::ServerScript
+            } else {
+                Middleware::ServerScriptDir
+            }
+        }
+        "LocalScript" => {
+            if inst.children().len() == 0 {
+                Middleware::ClientScript
+            } else {
+                Middleware::ClientScriptDir
+            }
+        }
+        "ModuleScript" => {
+            if inst.children().len() == 0 {
+                Middleware::ModuleScript
+            } else {
+                Middleware::ModuleScriptDir
+            }
+        }
+        _ => Middleware::Rbxmx,
     }
 }
 
@@ -119,11 +150,108 @@ fn syncback_dir<'new, 'old>(snapshot: &SyncbackSnapshot<'new, 'old>) -> Syncback
         }
     }
     let mut fs_snapshot = FsSnapshot::new().with_dir(&path);
-    // TODO metadata
+    // TODO metadata, including classname
 
     SyncbackReturn {
         inst_snapshot: InstanceSnapshot::from_instance(snapshot.new_inst()),
         fs_snapshot,
+        children,
+        removed_children,
+    }
+}
+
+fn syncback_project<'new, 'old>(
+    snapshot: &SyncbackSnapshot<'new, 'old>,
+) -> SyncbackReturn<'new, 'old> {
+    // We need to build a 'new' project and serialize it using an FsSnapshot.
+    // It's convenient to start with the old one though, since it means we have
+    // a thing to iterate through.
+    let mut project = Project::load_from_slice(
+        &snapshot.vfs().read(&snapshot.parent_path).unwrap(),
+        &snapshot.parent_path,
+    )
+    .unwrap();
+
+    let mut children = Vec::new();
+    let mut removed_children = Vec::new();
+
+    // Projects are special. We won't be adding or removing things from them,
+    // so we'll simply match Instances on a per-node basis and rebuild the tree
+    // with the new instance's data. This matching will be done by class and name
+    // to simplify things.
+    let mut nodes = vec![(
+        &mut project.tree,
+        snapshot.new_inst(),
+        snapshot.old_inst().unwrap(),
+    )];
+    while let Some((node, new_inst, old_inst)) = nodes.pop() {
+        let mut old_child_map = HashMap::with_capacity(old_inst.children().len());
+        for child_ref in old_inst.children() {
+            let child = snapshot.get_old_instance(*child_ref).unwrap();
+            old_child_map.insert(child.name(), child);
+        }
+        let mut new_child_map = HashMap::with_capacity(new_inst.children().len());
+        for child_ref in new_inst.children() {
+            let child = snapshot.get_new_instance(*child_ref).unwrap();
+            new_child_map.insert(child.name.as_str(), child);
+        }
+        for (child_name, child_node) in &mut node.children {
+            if let Some(new_child) = new_child_map.get(child_name.as_str()) {
+                if let Some(old_child) = old_child_map.get(child_name.as_str()) {
+                    // TODO verify class names
+                    for (name, value) in &new_child.properties {
+                        if child_node.properties.contains_key(name) {
+                            child_node
+                                .properties
+                                .insert(name.clone(), UnresolvedValue::from(value.clone()));
+                        }
+                    }
+                    nodes.push((child_node, new_child, *old_child));
+                    new_child_map.remove(child_name.as_str());
+                    old_child_map.remove(child_name.as_str());
+                } else {
+                    log::error!(
+                        "Node {} was in new tree but not old. How did we get here?",
+                        child_name
+                    );
+                }
+            } else {
+                panic!("Cannot add or remove children from a project")
+            }
+        }
+        // From this point, both maps contain only children of the current
+        // instance that aren't in the project. So, we just do some quick and
+        // dirty matching to identify children that were:
+        // - added (in new but not old)
+        // - removed (in old but not new)
+        for (new_name, new_child) in new_child_map {
+            if let Some(old_inst) = old_child_map.get(new_name) {
+                children.push(snapshot.from_parent(
+                    new_name,
+                    new_name.to_string(),
+                    new_child.referent(),
+                    Some(old_inst.id()),
+                ));
+                old_child_map.remove(new_name);
+            } else {
+                // it's new
+                children.push(snapshot.from_parent(
+                    new_name,
+                    new_name.to_string(),
+                    new_child.referent(),
+                    None,
+                ));
+            }
+        }
+        removed_children.extend(old_child_map.into_values());
+    }
+
+    SyncbackReturn {
+        inst_snapshot: InstanceSnapshot::from_instance(snapshot.new_inst()),
+        fs_snapshot: FsSnapshot::new().with_file(
+            &project.file_location,
+            serde_json::to_vec(&project).unwrap(),
+        ),
         children,
         removed_children,
     }
--- src/syncback/mod.rs
+++ src/syncback/mod.rs
@@ -3,21 +3,31 @@ mod middleware;
 
 use crate::{
     snapshot::{hash_tree, InstanceSnapshot, InstanceWithMeta, RojoTree},
-    snapshot_middleware::Middleware,
     Project,
 };
+use blake3::Hash;
 use memofs::Vfs;
 use rbx_dom_weak::{types::Ref, Instance, WeakDom};
-use std::path::{Path, PathBuf};
+use std::{
+    collections::{HashMap, VecDeque},
+    path::{Path, PathBuf},
+    rc::Rc,
+};
 
 pub use fs_snapshot::FsSnapshot;
 
-use self::middleware::syncback_middleware;
+use self::middleware::{get_best_middleware, syncback_middleware};
 
-#[derive(Debug)]
-pub struct SyncbackSnapshot<'new, 'old> {
+struct SyncbackData<'new, 'old> {
+    vfs: &'old Vfs,
     old_tree: &'old RojoTree,
     new_tree: &'new WeakDom,
+
+    old_hashes: Rc<HashMap<Ref, Hash>>,
+    new_hashes: Rc<HashMap<Ref, Hash>>,
+}
+pub struct SyncbackSnapshot<'new, 'old> {
+    data: Rc<SyncbackData<'new, 'old>>,
     old: Option<Ref>,
     new: Ref,
     parent_path: PathBuf,
@@ -36,8 +46,7 @@ impl<'new, 'old> SyncbackSnapshot<'new, 'old> {
         old_ref: Option<Ref>,
     ) -> Self {
         Self {
-            old_tree: self.old_tree,
-            new_tree: self.new_tree,
+            data: Rc::clone(&self.data),
             old: old_ref,
             new: new_ref,
             parent_path: self.parent_path.join(extension.as_ref()),
@@ -45,46 +54,64 @@ impl<'new, 'old> SyncbackSnapshot<'new, 'old> {
         }
     }
 
-    /// The 'old' Instance this snapshot is for, if it exists.
-    #[inline]
-    pub fn old_inst(&self) -> Option<InstanceWithMeta<'old>> {
-        self.old.and_then(|old| self.old_tree.get_instance(old))
-    }
-
-    /// The 'new' Instance this snapshot is for.
-    #[inline]
-    pub fn new_inst(&self) -> &'new Instance {
-        self.new_tree
-            .get_by_ref(self.new)
-            .expect("SyncbackSnapshot should not contain invalid referents")
-    }
-
     /// Returns an Instance from the old tree with the provided referent, if it
     /// exists.
     #[inline]
     pub fn get_old_instance(&self, referent: Ref) -> Option<InstanceWithMeta<'old>> {
-        self.old_tree.get_instance(referent)
+        self.data.old_tree.get_instance(referent)
     }
 
     /// Returns an Instance from the new tree with the provided referent, if it
     /// exists.
     #[inline]
     pub fn get_new_instance(&self, referent: Ref) -> Option<&'new Instance> {
-        self.new_tree.get_by_ref(referent)
+        self.data.new_tree.get_by_ref(referent)
+    }
+
+    /// The 'old' Instance this snapshot is for, if it exists.
+    #[inline]
+    pub fn old_inst(&self) -> Option<InstanceWithMeta<'old>> {
+        self.old
+            .and_then(|old| self.data.old_tree.get_instance(old))
+    }
+
+    /// The 'new' Instance this snapshot is for.
+    #[inline]
+    pub fn new_inst(&self) -> &'new Instance {
+        self.data
+            .new_tree
+            .get_by_ref(self.new)
+            .expect("SyncbackSnapshot should not contain invalid referents")
+    }
+
+    /// Returns the underlying VFS being used for syncback.
+    #[inline]
+    pub fn vfs(&self) -> &Vfs {
+        self.data.vfs
     }
 }
 
-pub fn syncback_loop(
-    vfs: &Vfs,
-    old_tree: &RojoTree,
-    new_tree: &WeakDom,
+pub fn syncback_loop<'old, 'new>(
+    vfs: &'old Vfs,
+    old_tree: &'old RojoTree,
+    new_tree: &'new WeakDom,
     project: &Project,
 ) -> anyhow::Result<Vec<(Ref, InstanceSnapshot)>> {
-    let old_hashes = hash_tree(old_tree.inner());
-    let new_hashes = hash_tree(new_tree);
-    let mut snapshots = vec![SyncbackSnapshot {
+    log::debug!("Hashing project DOM");
+    let old_hashes = Rc::new(hash_tree(old_tree.inner()));
+    log::debug!("Hashing file DOM");
+    let new_hashes = Rc::new(hash_tree(new_tree));
+
+    let syncback_data = Rc::new(SyncbackData {
+        vfs,
         old_tree,
         new_tree,
+        old_hashes: Rc::clone(&old_hashes),
+        new_hashes: Rc::clone(&new_hashes),
+    });
+
+    let mut snapshots = vec![SyncbackSnapshot {
+        data: syncback_data,
         old: Some(old_tree.get_root_id()),
         new: new_tree.root_ref(),
         parent_path: project.file_location.clone(),
@@ -98,6 +125,11 @@ pub fn syncback_loop(
         // skip reconciling them.
         if let Some(old_ref) = snapshot.old {
             if old_hashes.get(&old_ref) == new_hashes.get(&snapshot.new) {
+                log::debug!(
+                    "Skipping {} due to it being identically hashed as {:?}",
+                    get_inst_path(new_tree, snapshot.new),
+                    old_hashes.get(&old_ref)
+                );
                 continue;
             }
         }
@@ -106,6 +138,11 @@ pub fn syncback_loop(
             .old_inst()
             .and_then(|inst| inst.metadata().middleware)
             .unwrap_or_else(|| get_best_middleware(snapshot.new_inst()));
+        log::debug!(
+            "Middleware for {}: {:?}",
+            get_inst_path(new_tree, snapshot.new),
+            middleware
+        );
 
         let syncback = syncback_middleware(&snapshot, middleware);
 
@@ -113,40 +150,21 @@ pub fn syncback_loop(
             replacements.push((old_inst.parent(), syncback.inst_snapshot));
         }
 
+        log::debug!("Writing {} to vfs", get_inst_path(new_tree, snapshot.new));
         syncback.fs_snapshot.write_to_vfs(vfs)?;
 
-        // TODO handle children
+        snapshots.extend(syncback.children);
     }
 
     Ok(replacements)
 }
 
-fn get_best_middleware(inst: &Instance) -> Middleware {
-    match inst.class.as_str() {
-        "Folder" => Middleware::Dir,
-        // TODO this should probably just be rbxm
-        "Model" => Middleware::Rbxmx,
-        "Script" => {
-            if inst.children().len() == 0 {
-                Middleware::ServerScript
-            } else {
-                Middleware::ServerScriptDir
-            }
-        }
-        "LocalScript" => {
-            if inst.children().len() == 0 {
-                Middleware::ClientScript
-            } else {
-                Middleware::ClientScriptDir
-            }
-        }
-        "ModuleScript" => {
-            if inst.children().len() == 0 {
-                Middleware::ModuleScript
-            } else {
-                Middleware::ModuleScriptDir
-            }
-        }
-        _ => Middleware::Rbxmx,
+fn get_inst_path(dom: &WeakDom, referent: Ref) -> String {
+    let mut path: VecDeque<&str> = VecDeque::new();
+    let mut inst = dom.get_by_ref(referent);
+    while let Some(instance) = inst {
+        path.push_front(&instance.name);
+        inst = dom.get_by_ref(instance.parent());
     }
+    path.into_iter().collect::<Vec<&str>>().join(".")
 }
