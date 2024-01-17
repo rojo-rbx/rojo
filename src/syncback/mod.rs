01eee8a14a96c729d39ea0ea1018a49c6beeb36a
Micah <micah@uplift.games>
2024-01-17 14:23:50 -0800

    Move file name validation to own module Check if there are control characters in a name

--- src/syncback/mod.rs
+++ src/syncback/mod.rs
@@ -1,3 +1,4 @@
+mod file_names;
 mod fs_snapshot;
 mod snapshot;
 
@@ -17,6 +18,7 @@ use crate::{
     Project,
 };
 
+pub use file_names::is_valid_file_name;
 pub use fs_snapshot::FsSnapshot;
 pub use snapshot::{SyncbackData, SyncbackSnapshot};
 
@@ -231,39 +233,4 @@ fn get_inst_path(dom: &WeakDom, referent: Ref) -> String {
         inst = dom.get_by_ref(instance.parent());
     }
     path.into_iter().collect::<Vec<&str>>().join("/")
-}
-
-/// A list of file names that are not valid on Windows.
-const INVALID_WINDOWS_NAMES: [&str; 22] = [
-    "CON", "PRN", "AUX", "NUL", "COM1", "COM2", "COM3", "COM4", "COM5", "COM6", "COM7", "COM8",
-    "COM9", "LPT1", "LPT2", "LPT3", "LPT4", "LPT5", "LPT6", "LPT7", "LPT8", "LPT9",
-];
-
-/// A list of all characters that are outright forbidden to be included
-/// in a file's name.
-const FORBIDDEN_CHARS: [char; 9] = ['<', '>', ':', '"', '/', '|', '?', '*', '\\'];
-
-/// Returns whether a given name is a valid file name. This takes into account
-/// rules for Windows, MacOS, and Linux.
-///
-/// In practice however, these broadly overlap so the only unexpected behavior
-/// is Windows, where there are 22 reserved names.
-pub fn is_valid_file_name<S: AsRef<str>>(name: S) -> bool {
-    let str = name.as_ref();
-
-    if str.ends_with(' ') || str.ends_with('.') {
-        return false;
-    }
-    // TODO check control characters
-    for forbidden in FORBIDDEN_CHARS {
-        if str.contains(forbidden) {
-            return false;
-        }
-    }
-    for forbidden in INVALID_WINDOWS_NAMES {
-        if str == forbidden {
-            return false;
-        }
-    }
-    true
 }
