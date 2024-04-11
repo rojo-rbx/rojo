82fcbf0d2ec83da048f00dd5e7debb1ae74d32c7
Micah <micah@uplift.games>
2024-04-11 12:16:07 -0700

    Reorganize test order

--- tests/tests/syncback.rs
+++ tests/tests/syncback.rs
@@ -13,13 +13,13 @@ macro_rules! syncback_basic_test {
 
 syncback_basic_test! {
     baseplate,
-    respect_old_middleware,
-    nested_projects,
-    unscriptable_properties,
-    nested_projects_weird,
-    project_init,
     all_middleware,
-    ref_properties,
+    unscriptable_properties,
+    respect_old_middleware,
     rbxm_fallback
-    ref_properties_blank
+    project_init,
+    nested_projects,
+    nested_projects_weird,
+    ref_properties,
+    ref_properties_blank,
 }
