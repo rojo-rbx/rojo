d1f5a090fdf59d2f4bf06d50c898e17e26afacf3
Micah <micah@uplift.games>
2024-07-02 11:03:01 -0700

    Remove comment I forgot I left

--- src/syncback/ref_properties.rs
+++ src/syncback/ref_properties.rs
@@ -193,16 +193,3 @@ fn get_existing_id(inst: &Instance) -> Option<&str> {
         None
     }
 }
-
-/*
-When loading IDs we need to create a list and if there's a collision,
-cause it to have a stroke. If that works to catch the duplicates then we just
-need to re-serialize the IDs that collide. I think it's probably acceptable to
-just pick one of the two if we can't tell the difference between them and give
-the other a new ID. Maybe we emit a warning.
-
-This is gonna require a redo of the linking a bit, since it'll mean that we
-can't just blindly use the old IDs. The plus side is that it means we can
-collect these in a way that isn't awful via mapping an ID to a referent, so
-it may end up improving the overall quality of the above code.
-*/
