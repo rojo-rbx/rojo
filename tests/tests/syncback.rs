13564c97fbe3868ec0130fbef9c817640206ffda
Micah <micah@uplift.games>
2025-05-29 15:22:22 -0700

    Update a few tests to see how it looks

--- tests/tests/syncback.rs
+++ tests/tests/syncback.rs
@@ -1,16 +1,31 @@
-use std::fs;
+use std::{ffi::OsStr, fs};
 
 use insta::assert_snapshot;
 
-use crate::rojo_test::syncback_util::run_syncback_test;
+use crate::rojo_test::syncback_util::{run_syncback_test, snapshot_rbxm};
 
-#[test]
-fn unscriptable_properties() {
-    run_syncback_test("unscriptable_properties", |path| {
-        let project_path = path.join("default.project.json");
-        let content =
-            fs::read_to_string(project_path).expect("could not read default.project.json");
+macro_rules! syncback_tests {
+    ($($test_name:ident => $list:expr$(,)?),*) => {$(
+        #[test]
+        fn $test_name() {
+            run_syncback_test(stringify!($test_name), |path| {
+                for name in $list {
+                    let snapshot_name = format!(concat!(stringify!($test_name), "-{}"), name);
+                    let new = path.join(name);
+                    if let Some("rbxm") = new.extension().and_then(OsStr::to_str) {
+                        let content = fs::read(new).unwrap();
+                        snapshot_rbxm(&snapshot_name, content);
+                    } else {
+                        let content = fs::read_to_string(new).unwrap();
+                        assert_snapshot!(snapshot_name, content);
+                    }
+                }
+            });
+        }
+    )*};
+}
 
-        assert_snapshot!("unscriptable_properties-default.project.json", content)
-    });
+syncback_tests! {
+    string_value_project => ["default.project.json"],
+    unscriptable_properties => ["default.project.json"],
 }
