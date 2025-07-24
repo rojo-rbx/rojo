0fa87e107ca0b90618d949ef5011e0e69dc7b890
sebastian2852 <sebastian2852@proton.me>
2025-07-24 12:29:09 +0200

    Basic workaround to confirm cause

--- plugin/src/PatchTree.lua
+++ plugin/src/PatchTree.lua
@@ -234,6 +234,9 @@ function PatchTree.build(patch, instanceMap, changeListHeaders)
 
 			local changeIndex = 0
 			local function addProp(prop: string, current: any?, incoming: any?, metadata: any?)
+				if current == nil and incoming == nil then
+					return
+				end
 				changeIndex += 1
 				changeList[changeIndex] = { prop, current, incoming, metadata }
 			end
