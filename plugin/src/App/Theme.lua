4da4fa6411b48260f2c21b06d68003e38234d379
sebastian2852 <sebastian2852@proton.me>
2025-07-30 21:51:57 +0200

    Ran stylua

--- plugin/src/App/Theme.lua
+++ plugin/src/App/Theme.lua
@@ -173,11 +173,11 @@ function StudioProvider:updateTheme()
 			Warning = studioTheme:GetColor(Enum.StudioStyleGuideColor.WarningText),
 
 			Text = {
-				Add = if isDark then Color3.new(0, 0, 0) else Color3.new(1, 1, 1);
-				Remove = if isDark then Color3.new(0, 0, 0) else Color3.new(1, 1, 1);
-				Edit = if isDark then Color3.new(0, 0, 0) else Color3.new(1, 1, 1);
-				Remain = studioTheme:GetColor(Enum.StudioStyleGuideColor.MainText); -- FROM: theme.Settings.Setting.DescriptionColor
-			}
+				Add = if isDark then Color3.new(0, 0, 0) else Color3.new(1, 1, 1),
+				Remove = if isDark then Color3.new(0, 0, 0) else Color3.new(1, 1, 1),
+				Edit = if isDark then Color3.new(0, 0, 0) else Color3.new(1, 1, 1),
+				Remain = studioTheme:GetColor(Enum.StudioStyleGuideColor.MainText), -- FROM: theme.Settings.Setting.DescriptionColor
+			},
 		},
 		ConnectionDetails = {
 			ProjectNameColor = studioTheme:GetColor(Enum.StudioStyleGuideColor.BrightText),
