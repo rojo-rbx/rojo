a30a8ecd0f757a23c7d5be5f91e8a4d55d87b8fc
Micah <git@dekkonot.com>
2026-07-05 23:15:34 -0700

    Lock 'Check for Updates' setting if user blocks access to `api.github.com` (#1297)

--- plugin/src/Version.lua
+++ plugin/src/Version.lua
@@ -111,6 +111,24 @@ Version._cachedLatestCompatible = nil :: {
 	timestamp: number,
 }?
 
+--[[
+	A user may choose to reject requests to api.github.com. If they do, we want
+	to disable the setting for checking for updates to indicate that it does
+	nothing.
+]]
+Version._apiBlocked = nil :: boolean?
+
+function Version.isApiBlocked(): boolean
+	if Version._apiBlocked == nil then
+		local isLocalInstall = string.find(debug.traceback(), "\n[^\n]-user_.-$") ~= nil
+		Version.retrieveLatestCompatible({
+			version = Config.version,
+			includePrereleases = isLocalInstall and Settings:get("checkForPrereleases"),
+		})
+	end
+	return Version._apiBlocked
+end
+
 function Version.retrieveLatestCompatible(options: {
 	version: { number },
 	includePrereleases: boolean?,
@@ -136,8 +154,13 @@ function Version.retrieveLatestCompatible(options: {
 		:await()
 
 	if success == false or type(releases) ~= "table" or next(releases) ~= 1 then
+		-- Roblox's HTTP errors are weird!
+		if string.find(tostring(releases), "^Unknown HTTP error: HttpService permission denied") then
+			Version._apiBlocked = true
+		end
 		return nil
 	end
+	Version._apiBlocked = false
 
 	-- Iterate through releases, looking for the latest compatible version
 	local latestCompatible: LatestReleaseInfo? = nil
